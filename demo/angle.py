aa4d278828b7c200e77a40cc837174ca564b9a40
James-Durant <James.Durant@warwick.ac.uk>
2021-06-17 16:11:42 +0100

    Updated demo

--- demo/angle.py
+++ demo/angle.py
@@ -4,6 +4,8 @@ import sys
 sys.path.append('../experimental-design')
 
 from refnx.reflect import SLD
+
+from structures import Sample
 from design import angle_choice_single
 
 # Defines a structure describing a simple sample.
@@ -15,19 +17,22 @@ substrate = SLD(2.047, name='Substrate')(thick=0, rough=2)
 structure = air | layer1 | layer2 | substrate
 structure.name = 'thin_layer_sample'
 
+# Wrap the refnx structure in the custom Sample class.
+sample = Sample(structure)
+
 # Path to directory to save results to.
 save_path = './results'
 
 points = 70 # Number of data points to simulate.
-time = 20 # Time to use for simulation.
+time = 40 # Time to use for simulation.
 
 # Angles (in degrees) to calculate over.
 angle_range = np.linspace(0.2, 2.3, 200)
 
 # Investigate first angle choice.
 initial_angle_times = {}
-angle_choice_single(structure, initial_angle_times, angle_range, points, time, save_path, 'angle_choice_1')
+angle_choice_single(sample, initial_angle_times, angle_range, points, time, save_path, 'angle_choice_1')
 
 # Investigate second angle choice.
-initial_angle_times = {2.3: (70, 20)} # First first angle choice - Angle: (Points, Time)
-angle_choice_single(structure, initial_angle_times, angle_range, points, time, save_path, 'angle_choice_2')
+initial_angle_times = {2.3: (70, 40)} # First first angle choice - Angle: (Points, Time)
+angle_choice_single(sample, initial_angle_times, angle_range, points, time, save_path, 'angle_choice_2')
